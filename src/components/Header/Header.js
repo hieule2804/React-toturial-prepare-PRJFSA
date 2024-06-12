eac7a0b455969f9f28e64ef5a2090a494e226e63
hieule2804 <letrunghieu28042003@gmail.com>
2024-06-12 23:41:54 +0700

    index route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Ziwllw App</Navbar.Brand>
+                <NavLink to="/" className='navbar-brand'>Ziwllw App</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -21,13 +21,13 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Setting" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item >Login</NavDropdown.Item>
+                            <NavDropdown.Item >
                                 Logout
                             </NavDropdown.Item>
                             {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item> */}
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
+                            <NavDropdown.Item >
                                 Profile
                             </NavDropdown.Item>
                         </NavDropdown>
